632aa67a382fa0e287f36dc312db7d61ad78c4c0
MarineGsell <56953940+MarineGsell@users.noreply.github.com>
2025-06-17 15:53:48 +0200

    favicon

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <Header />
+    <NuxtPage />
+  </div>
+</template>
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+</style>
